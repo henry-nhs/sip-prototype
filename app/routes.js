5ee1fe91393805c294dea8c6f8b3cbb641a8fbc9
Henry Neves-Charge <henry.neves-charge@nhs.net>
2020-11-11 15:51:38 +0000

    Eligibility checker

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,46 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.get('/eligible-1-backend', function (req, res) {
+    switch (req.session.data['elig-employed']) {
+        case "unemployed":
+            res.redirect('not-eligible');
+            break;
+        default:
+            //   req.session.data['diversity-parents-completed'] = "true";
+            res.redirect('eligible-2');
+            break;
+    }
+});
+
+router.get('/eligible-2-backend', function (req, res) {
+    switch (req.session.data['elig-wfh']) {
+        case "yes":
+            res.redirect('not-eligible');
+            break;
+        default:
+            res.redirect('eligible-3');
+            break;
+    }
+});
+
+router.get('/eligible-3-backend', function (req, res) {
+    switch (req.session.data['elig-losing-income']) {
+        case "no":
+            res.redirect('not-eligible');
+            break;
+        default:
+            res.redirect('eligible-4');
+            break;
+    }
+});
+
+router.get('/eligible-4-backend', function (req, res) {
+    if(req.session.data['elig-benefits'].includes('no-benefit')) {
+            res.redirect('not-eligible');
+    } else {
+        res.redirect('may-be-eligible');
+    }
+});
+
 module.exports = router;
