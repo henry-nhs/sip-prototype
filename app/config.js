33b660ff349fafa2c412ac2a092be1299d96c457
Henry Neves-Charge <henry.neves-charge@nhs.net>
2020-11-11 08:31:35 +0000

    Basic changes

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Claim Test & Trace Support Payment',
 
   // Port to run nodemon on locally
   port: 2000,
